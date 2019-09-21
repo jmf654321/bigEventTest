cfd6a0ffae38dc18aeb0e836d6d07a061998f165
jimeifang123 <2543555614@qq.com>
2019-09-21 19:44:52 +0800

    第二次提交获取用户名信息

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -2,4 +2,5 @@ var userURl='http://localhost:8000';
 userUrls={
     userLoginUrl:userURl+'/admin/login',
     userLogoutUrl:userURl+'/admin/logout',
+    usergetUserUrl:userURl+'/admin/getuser'
 }
--- web_back/js/utils/user.js
+++ web_back/js/utils/user.js
@@ -5,8 +5,16 @@ function userLogin(userName,password,callback){
         callback(res);
     })
 }
+// 管理员退出
 function userLogout(callback){
   $.post(userUrls.userLogoutUrl,function(res){
+    callback(res);
+
+  })
+}
+//获取用户信息
+function getUserMsg(callback){
+  $.get(userUrls.usergetUserUrl,function(res){
     callback(res);
 
   })
