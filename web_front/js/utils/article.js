028353345a82a477cd24b06feab437a93d818a3d
jimeifang123 <2543555614@qq.com>
2019-09-26 20:34:10 +0800

    游客首页，评论显示和添加功能实现

--- web_front/js/utils/article.js
+++ web_front/js/utils/article.js
@@ -4,38 +4,5 @@ var article ={
             callback(res);
         })
     },
-    del:function(id,callback){
-        $.get(userUrls.article_del,{id:id},function(res){
-            callback(res);
-        })
-    },
-    publish:function(fd,callback){
-        $.ajax({
-            url:userUrls.article_publish,
-            type:'post',
-            data:fd,
-            processData:false,
-            contentType:false,
-            success:function(res){
-                callback(res)
-            }
-
-        })
-
-    },
-    edit:function(fd,callback){
-        $.ajax({
-            url:userUrls.article_edit,
-            type:'post',
-            data:fd,
-            processData:false,
-            contentType:false,
-            success:function(res){
-                callback(res)
-            }
-
-        })
-
-    }
-
+    
 }
--- web_front/js/utils/config.js
+++ web_front/js/utils/config.js
@@ -1,19 +1,11 @@
 var userURl='http://localhost:8000';
 userUrls={
-    userLoginUrl:userURl+'/admin/login',
-    userLogoutUrl:userURl+'/admin/logout',
-    usergetUserUrl:userURl+'/admin/getuser',
-
-    category_show:userURl+'/admin/category_search',
-    category_add:userURl+'/admin/category_add', 
-    category_edit:userURl+'/admin/category_edit', 
-    category_del:userURl+'/admin/category_delete',
-
     article_show:userURl+'/admin/search', 
-    article_del:userURl+'/admin/article_delete',
-    article_add:userURl+'/admin/category_add',
-    article_publish:userURl+'/admin/article_publish',
-    article_edit:userURl+'/admin/article_edit',
+
+    comment_show:userURl+'/get_comments',
+    comment_add:userURl+'/post_comment',
+
+    
 
 
 
